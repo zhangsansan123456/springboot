17f1a5e4fea641cc50049f037063abee36b01eec
张三 <qq1219252138@163.com>
2020-09-05 08:52:02 +0800

    练习git idea提交

--- src/main/java/com/atguigu/MyApplication.java
+++ src/main/java/com/atguigu/MyApplication.java
@@ -9,5 +9,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class MyApplication {
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class,args);
+        System.out.println("1111111111111");
     }
 }
