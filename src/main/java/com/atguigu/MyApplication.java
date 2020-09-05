02e4bd99219518eeb96567750459c32649baade0
张三 <qq1219252138@163.com>
2020-09-05 09:09:43 +0800

    练习git idea提交master

--- src/main/java/com/atguigu/MyApplication.java
+++ src/main/java/com/atguigu/MyApplication.java
@@ -9,6 +9,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class MyApplication {
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class,args);
-        System.out.println("1111111111111 master");
+        System.out.println("master");
     }
 }
